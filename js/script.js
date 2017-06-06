fa01c015a3d0d0a0b63b56c6ec87308e62e13482
habudala <habudala@live.com>
2017-06-06 01:33:13 -0400

    Added JavaScript
    Added animated navigation, amongst other things.

--- js/script.js
+++ js/script.js
@@ -44,7 +44,7 @@ function script () {
 		        break;
 		    case "10":transformation (subject,subjectId);
 		    		  setDesc(subject);
-		    		  setCont ("Digital HR", "#ee2a7b;");		        
+		    		  setCont ("Digital HR", "#ee2a7b");		        
 		        break;
 		    case "11":transformation (subject,subjectId);
 		    		  setDesc(subject);
@@ -139,32 +139,125 @@ function script () {
 		  var descCont = document.getElementById("descCont");
 		  descCont.innerHTML = content;
 		  descCont.style.color = color;
-		  descCont.style.lineHeight = "3vw";
+		  descCont.style.lineHeight = "3.5vw";
 		    
 	}
 
-	// function sg () {
-		var imgArray = document.querySelectorAll("#icons>li>img");
-		var z = 0; 
 
-				setInterval(function(){ 
-				// console.log(z);
-			 	transformation (imgArray[z],imgArray[z].id);
-			 	var color = imgArray[z].getAttribute("myThing");
-			 	var desc = imgArray[z].getAttribute("name");
-			 	setCont (desc, color);
+	var imgArray = document.querySelectorAll("#icons>li>img");
+	var z = 0; 
 
+		setInterval(function(){ 
+			// console.log(z);
+			transformation (imgArray[z],imgArray[z].id);
+			var color = imgArray[z].getAttribute("myThing");
+			var desc = imgArray[z].getAttribute("name");
+			setCont (desc, color);
 
-
-			 	setTimeout (function(){ 
-			 	transformBack(imgArray[z],imgArray[z].id);
-			 	if (z < 14) {z++;} else {z = 0;}
-			 	},700);
-			 }, 1000); 
+			setTimeout (function(){ 
+			transformBack(imgArray[z],imgArray[z].id);
+			if (z < 14) {z++;} else {z = 0;}
+			},1500);
+		}, 2500); 
 
 	var iconLine = document.getElementById("icons");
+	var iconLine = document.getElementById("menuIconCont");
 	iconLine.addEventListener("mouseover",highlight, false);
 	iconLine.addEventListener("mouseout",deHighlight, false);
+	// menuIconCont.addEventListener("click",menuOut, false);
 
+
+	// /////////////////// ///////// /// ///////////////
+
+////////////////////Mobile Menu Slide-Out Animation////////////////////
+
+var myArr = document.querySelectorAll("#mbNav>ul>li");
+// console.log(listArr);// selfcheck
+var modal = document.getElementById("myModal");
+//slideout bg
+var mdCont = document.getElementById("mdCont");
+// Get button that opens the modal
+var btn = document.getElementById("menuIconCont");
+//get menu lines to get them to disappear later
+var line1 =document.getElementById("line1");
+// menu-in/out counter
+var counter = 0;
+
+btn.onclick = function() {
+
+	if(counter == 0) {
+
+		// animate menu icon
+	    line1.style.opacity = 0;
+
+	    //animate modal window
+		modal.style.display = "block";
+		mdCont.style.animationName = "animate";
+		mdCont.style.animationDuration = "1.5s";	
+    	counter=1;
+
+    	// animate list items
+    	if(myArr.length>0) {
+    		var laz = 0;
+
+			function repeat () {
+				// console.log(myArr.item(laz)); //self-check
+				myArr.item(laz).style.marginLeft = "0px";
+				myArr.item(laz).style.transition = "margin-left 1s linear";
+				laz++;
+				if (laz >= myArr.length) { clearInterval(timer);}
+			}
+
+			var timer = window.setInterval(repeat,200);
+
+   		}
+
+	}else if (counter == 1) {
+
+		if(myArr.length>0) {
+    		var laz = 0;
+    		 line1.style.opacity = 1;
+			function repeat () {
+				// console.log(myArr.item(laz)); //self-check
+				myArr.item(laz).style.marginLeft = "100vw";
+				myArr.item(laz).style.transition = "margin-left 1s linear";
+				laz++;
+				if (laz >= myArr.length) { clearInterval(timer);
+					mdCont.style.animationName = "animateBack";
+		mdCont.style.animationDuration = "3s";
+		setTimeout(function(){ modal.style.display = "none";},1700);}
+			}
+
+			var timer = window.setInterval(repeat,200);
+
+   		}
+
+		// mdCont.style.animationName = "animateBack";
+		// mdCont.style.animationDuration = "3s";
+		// setTimeout(function(){ modal.style.display = "none";},5000);
+
+		
+
+    	counter=0;
+	}
+   
+}
+
+////My Own "Individual-element-slide-in" method written by Laszlo Habuda c.2016 AD//////
+/////////////////////////////////"Use it wisely young padawan!"////////////////
+		// var myArr = document.querySelectorAll("#dtNav>ul>li");
+		// var i = 0;
+
+		// function repeat () {
+		// 	myArr[i].style.transition = "margin-left 3s ease-in-out 1s";
+		// 	myArr[i].style.marginLeft = "0px";
+		// 	i++;
+		// 	if (i > myArr.length) {i = 0;}
+		// }
+
+		// var timer = window.setInterval(repeat,1000);
+////////////////////////////////////////////////////////////////////
+
+ 
 }
 window.onload = script;
