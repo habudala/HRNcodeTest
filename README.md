a3986df73412fdc1de0165f7df9913d959caaa12
habudala <habudala@live.com>
2017-06-06 03:15:21 -0400

    Final Touches
    Cleaning up, Creating separate reset stylesheet, creating README file.

---
+++ README.md
@@ -0,0 +1,9 @@
+## I tried to get as much done in the one week time frame as i could.
+## There were some elements I did not have enough time to devote to:
+### - logo animation (the twitching)
+### - dropdown menu transition
+### - dropdown mini menu itmes
+
+##The website was built to be fully responsive from 640px to 3900px
+##As I was working, I was comminting my changes to my github repository,
+## so you may track my progress (as requested in directions).
--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 function script () {
 
+//////////// When someone hovers over the svg icon///////////////
 	function highlight (e) {
 		var subject = e.target;
 		var subjectId = subject.id;
-		// console.log(subjectId); 
 
 		switch(subjectId) {
 		    case "1": transformation (subject,subjectId);
@@ -71,11 +71,10 @@ function script () {
 		}
 
 	}
-
+// //////////////////when someone stops hovering over the svg icon/////
 	function deHighlight (e) {
 		var subject = e.target;
 		var subjectId = subject.id;
-		// console.log(subjectId); 
 
 		switch(subjectId) {
 		    case "1": transformBack(subject,subjectId); break;
@@ -99,6 +98,32 @@ function script () {
 
 	}
 
+// //////////////drop down menu animation//////////////
+
+///////////////faulty!!! needs checking --> no transition//////////
+////////////////////down//////////////
+
+	function dDown (){
+		var drDown = document.getElementById("drDown");
+		var myId = document.getElementById("myId");
+		drDown.style.display="block";
+		myId.style.opacity="1";
+		myId.style.left="75vw";
+
+	}
+/////////////////up/////////////////
+
+	function goUp (){
+		var drDown = document.getElementById("drDown");
+		var myId = document.getElementById("myId");
+		drDown.style.display="none";
+		myId.style.left="100vw";
+		myId.style.opacity="0";
+
+	}
+
+	// /////////-setting the description right in the middle of svg///////////
+
 	function setDesc(element) {
 		var desc = document.getElementById("statDesc");
 		var imgInfo =  element.getBoundingClientRect();
@@ -118,7 +143,7 @@ function script () {
 		return 
 
 	}
-
+	/////////    transforming svg icon and comment box//////
 	function transformation (image,imgId) {
 		   var source = image.src = "images/icon" + imgId + "(1).svg";
 		    image.style.transform = "scale(1.5,1.5)";
@@ -143,6 +168,7 @@ function script () {
 		    
 	}
 
+// -------------------------creating image list loop so icons get highlighted---------------
 
 	var imgArray = document.querySelectorAll("#icons>li>img");
 	var z = 0; 
@@ -150,8 +176,8 @@ function script () {
 		setInterval(function(){ 
 			// console.log(z);
 			transformation (imgArray[z],imgArray[z].id);
-			var color = imgArray[z].getAttribute("myThing");
-			var desc = imgArray[z].getAttribute("name");
+			var color = imgArray[z].getAttribute("myThing"); // find color in html associated with icon
+			var desc = imgArray[z].getAttribute("name"); // find name of image instead of if-else or switch statement
 			setCont (desc, color);
 
 			setTimeout (function(){ 
@@ -160,8 +186,13 @@ function script () {
 			},1500);
 		}, 2500); 
 
+///////////////////////////EVENT LISTENERS FOR FUNCTIONS ABOVE///////////
+
 	var iconLine = document.getElementById("icons");
 	var iconLine = document.getElementById("menuIconCont");
+	var dropDown = document.getElementById("down");
+	dropDown.addEventListener("mouseover",dDown, false);
+	dropDown.addEventListener("mouseout",goUp, false);
 	iconLine.addEventListener("mouseover",highlight, false);
 	iconLine.addEventListener("mouseout",deHighlight, false);
 	// menuIconCont.addEventListener("click",menuOut, false);
@@ -185,7 +216,7 @@ var counter = 0;
 
 btn.onclick = function() {
 
-	if(counter == 0) {
+	if(counter == 0) {   // if menu is not visible
 
 		// animate menu icon
 	    line1.style.opacity = 0;
@@ -197,8 +228,9 @@ btn.onclick = function() {
     	counter=1;
 
     	// animate list items
-    	if(myArr.length>0) {
-    		var laz = 0;
+    	if(myArr.length>0) {  // checking if there are list items to display
+    		
+    		var laz = 0;   //array iterator laz = 1,2,3,4, etc..
 
 			function repeat () {
 				// console.log(myArr.item(laz)); //self-check
@@ -212,15 +244,15 @@ btn.onclick = function() {
 
    		}
 
-	}else if (counter == 1) {
+	}else if (counter == 1) {    // if menu visible
 
-		if(myArr.length>0) {
+		if(myArr.length>0) {    // checking if there are list items to display
     		var laz = 0;
     		 line1.style.opacity = 1;
 			function repeat () {
 				// console.log(myArr.item(laz)); //self-check
-				myArr.item(laz).style.marginLeft = "100vw";
-				myArr.item(laz).style.transition = "margin-left 1s linear";
+				myArr.item(laz).style.marginLeft = "100vw";    //   animating list elements 
+				myArr.item(laz).style.transition = "margin-left 1s linear"; // animating list elements
 				laz++;
 				if (laz >= myArr.length) { clearInterval(timer);
 					mdCont.style.animationName = "animateBack";
@@ -232,32 +264,10 @@ btn.onclick = function() {
 
    		}
 
-		// mdCont.style.animationName = "animateBack";
-		// mdCont.style.animationDuration = "3s";
-		// setTimeout(function(){ modal.style.display = "none";},5000);
-
-		
-
     	counter=0;
 	}
    
 }
 
-////My Own "Individual-element-slide-in" method written by Laszlo Habuda c.2016 AD//////
-/////////////////////////////////"Use it wisely young padawan!"////////////////
-		// var myArr = document.querySelectorAll("#dtNav>ul>li");
-		// var i = 0;
-
-		// function repeat () {
-		// 	myArr[i].style.transition = "margin-left 3s ease-in-out 1s";
-		// 	myArr[i].style.marginLeft = "0px";
-		// 	i++;
-		// 	if (i > myArr.length) {i = 0;}
-		// }
-
-		// var timer = window.setInterval(repeat,1000);
-////////////////////////////////////////////////////////////////////
-
- 
 }
 window.onload = script;
